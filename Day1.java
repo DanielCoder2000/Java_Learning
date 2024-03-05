3d37069a177be47b4c111bb76c65c4bf386a81d9
Letsatsi D. Mothemane <111023181+DanielCoder2000@users.noreply.github.com>
2024-03-05 12:22:10 +0200

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Day1;
+
+/**
+ *
+ * @author Mmirwa Mothemane
+ */
+public class Day1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+        /* Java Data Types (Typed in pascal naming convention)*/
+         //Variables and constants
+ int age =24;
+ String name="Letsatsi";
+ System.out.println(age);
+ System.out.println(name);
+ //Primitive 
+ byte number1=127;
+ short number2=31999;
+ int number3= 299999999;
+ long number4=3_856_956_586L;
+ float number5=100.88F;
+ double number6=2885.369;
+ char letter = 'G';
+ boolean choice=false;
+ 
+ System.out.println(number1);
+ System.out.println(number2);
+ System.out.println(number3);
+ System.out.println(number4);
+ System.out.println(number5);
+ System.out.println(number6);
+ System.out.println(letter);
+ System.out.println(choice);
+    }
+    
+}
